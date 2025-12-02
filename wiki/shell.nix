ee7d15c75c692413053c8f95ebf9b3cfef4345be
Marc Zweiler <marc.zweiler@outlook.at>
2025-12-02 23:25:22 +0100

    Added `shell.nix` to fixate mdbook versions to make them non-breakable for the future

---
+++ wiki/shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import (builtins.fetchTarball {
+  # Pin nixpkgs to the 25.05 release tarball from GitHub.
+  # If your channel uses a different ref name, replace "nixos-25.05" below.
+  url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.05.tar.gz";
+}) {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.mdbook
+    pkgs.mdbook-toc
+    pkgs.mdbook-katex
+  ];
+}
